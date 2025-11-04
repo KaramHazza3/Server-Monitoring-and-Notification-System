ca9addf04a98a8ee3aa11d36fffa298194889da7
karamhazza3 <karam.hazzaa@gmail.com>
2025-11-04 19:05:58 +0200

    Add mongoDb repository

---
+++ MonitoringAndNotificationSystem.MessageProcessingAndAnomalyDetectionService/Repositories/ServerStatisticsMongoDbRepository.cs
@@ -0,0 +1,29 @@
+﻿using MessageProcessingAndAnomalyDetectionService.Contexts;
+using MessageProcessingAndAnomalyDetectionService.Data;
+using MessageProcessingAndAnomalyDetectionService.Repositories.Intf;
+using MongoDB.Driver;
+
+namespace MessageProcessingAndAnomalyDetectionService.Repositories;
+
+public class ServerStatisticsMongoDbRepository : IServerStatisticsRepository
+{
+    private readonly MongoDbContext _context;
+
+    public ServerStatisticsMongoDbRepository(MongoDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(ServerStatistics serverStatistics)
+    {
+        await this._context.ServerStatistics.InsertOneAsync(serverStatistics);
+    }
+
+    public async Task<ServerStatistics?> GetLastAsync()
+    {
+        return await this._context.ServerStatistics
+            .Find(FilterDefinition<ServerStatistics>.Empty)
+            .SortByDescending(s => s.Timestamp)
+            .FirstOrDefaultAsync();
+    }
+}
