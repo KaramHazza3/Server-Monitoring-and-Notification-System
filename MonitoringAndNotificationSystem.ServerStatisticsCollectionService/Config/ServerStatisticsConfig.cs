a5f42c5a164bc93cbf0bd14aad6201e1e74661cb
karamhazza3 <karam.hazzaa@gmail.com>
2025-11-04 19:00:04 +0200

    Add configs

---
+++ MonitoringAndNotificationSystem.ServerStatisticsCollectionService/Config/ServerStatisticsConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace ServerStatisticsCollectionService.Config;
+
+public class ServerStatisticsConfig
+{
+    public int SamplingIntervalSeconds { get; set; }
+    public string ServerIdentifier { get; set; } = string.Empty;
+}
+    
