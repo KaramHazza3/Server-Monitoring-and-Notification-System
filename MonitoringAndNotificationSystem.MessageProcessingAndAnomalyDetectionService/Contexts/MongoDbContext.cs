41b5f1eb0610d32b82c9cb736488bc00993d9bf7
karamhazza3 <karam.hazzaa@gmail.com>
2025-11-04 19:06:15 +0200

    Add mongodb context

---
+++ MonitoringAndNotificationSystem.MessageProcessingAndAnomalyDetectionService/Contexts/MongoDbContext.cs
@@ -0,0 +1,21 @@
+﻿using MessageProcessingAndAnomalyDetectionService.Config;
+using MessageProcessingAndAnomalyDetectionService.Data;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace MessageProcessingAndAnomalyDetectionService.Contexts;
+
+public class MongoDbContext
+{
+    private readonly IMongoDatabase _mongoDatabase;
+    private readonly MongoDbConfig _mongoDbConfig;
+    
+    public MongoDbContext(IOptions<MongoDbConfig> mongoDbConfig)
+    {
+        _mongoDbConfig = mongoDbConfig.Value;
+        var client = new MongoClient(_mongoDbConfig.ConnectionString);
+        _mongoDatabase = client.GetDatabase(_mongoDbConfig.DatabaseName);
+    }
+
+    public IMongoCollection<ServerStatistics> ServerStatistics => _mongoDatabase.GetCollection<ServerStatistics>("ServerStatistics");
+}
