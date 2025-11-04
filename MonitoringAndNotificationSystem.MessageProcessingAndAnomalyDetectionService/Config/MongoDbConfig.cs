d30d86ceac2834db289ce96f570f9495bf2d5ef6
karamhazza3 <karam.hazzaa@gmail.com>
2025-11-04 19:05:26 +0200

    Add configurations to read from appsettings

---
+++ MonitoringAndNotificationSystem.MessageProcessingAndAnomalyDetectionService/Config/MongoDbConfig.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace MessageProcessingAndAnomalyDetectionService.Config;
+
+public class MongoDbConfig
+{
+    public string ConnectionString { get; set; } = string.Empty;
+    public string DatabaseName { get; set; } = string.Empty;
+}
---
+++ MonitoringAndNotificationSystem.MessageProcessingAndAnomalyDetectionService/Config/SignalRConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace MessageProcessingAndAnomalyDetectionService.Config;
+
+public class SignalRConfig
+{
+    public string SignalRUrl { get; set; } = string.Empty;
+    public string HubUrl { get; set; } = string.Empty;
+}
